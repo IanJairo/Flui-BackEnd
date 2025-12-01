793de0e6a18f76fb226387e7186342367006217f
Ian Jairo <ijtg.ian@gmail.com>
2025-12-01 19:15:07 -0300

    Update: Valor da main

--- src/app.js
+++ src/app.js
@@ -104,7 +104,7 @@ app.use('/api/auth', require('./routes/authRoutes.js'));
 
 app.listen(port, async () => {
   console.log('='.repeat(60));
-  console.log('Hello, Good Night  - Teste de Integração');
+  console.log('Hola, buenas noches  -');
   console.log('='.repeat(60));
   console.log(`Servidor rodando na porta ${port}`);
   console.log(`Ambiente: ${process.env.NODE_ENV || 'development'}`);
